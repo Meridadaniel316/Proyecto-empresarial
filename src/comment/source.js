9ac752d531994404cfd9bef3f316342d03d16d9c
MeridaDaniel <Dacastamerd@gmail.com>
2021-07-01 22:43:05 -0500

    Se tradujo el document source.js a español

--- src/comment/source.js
+++ src/comment/source.js
@@ -1,10 +1,10 @@
 export default function buildMakeSource ({ isValidIp }) {
   return function makeSource ({ ip, browser, referrer } = {}) {
     if (!ip) {
-      throw new Error('Comment source must contain an IP.')
+      throw new Error('La fuente del comentario debe tener una IP.)
     }
     if (!isValidIp(ip)) {
-      throw new RangeError('Comment source must contain a valid IP.')
+      throw new RangeError('La fuente del comentario debe contener una IP valida.')
     }
     return Object.freeze({
       getIp: () => ip,
